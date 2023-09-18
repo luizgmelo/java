bf3900f70abb30059d322cb28cf6fa40e0ca6afc
Luiz Guilherme <luizgmelo64@gmail.com>
2023-09-18 11:20:17 -0300

    add hello goodbye program

---
+++ hello/HelloGoodbye.java
@@ -0,0 +1,10 @@
+public class HelloGoodbye {
+    public static void main(String args[]) {
+      String firstPerson;
+      String secondPerson;
+      firstPerson = args[0];
+      secondPerson = args[1];
+      System.out.println("Hello " + firstPerson + " and " + secondPerson + ".");
+      System.out.println("Goodbye " + firstPerson + " and " +secondPerson + ".");
+    }
+}
